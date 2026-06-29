8b01ab0a401e5cced83ca436bd2510922c1a820b
Charlie-Patrickson <139253262+Charlie-Patrickson@users.noreply.github.com>
2026-06-29 17:30:55 +0200

    Add files via upload

---
+++ Figs/Fig_5a_GIF_script.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 28 19:49:22 2025
+
+@author: cp728
+"""
+
+import Bloch_sphere_functions as BSfcs
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import imageio.v2 as imageio
+import os
+
+from matplotlib import rcParams
+plt.rcParams['mathtext.fontset'] = 'dejavuserif'
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.sans-serif'] = ['Cambria math', 'sans-serif']
+
+output_dir = "frames"
+os.makedirs(output_dir, exist_ok=True)  # Only creates it if it doesn't already exist
+
+Skip_lines = 1
+linewidth = 2
+linestyle = "solid"
+arrowhead_size = 24
+Signal_Max_Y = 0.75
+drive_colour_z = "#E78C07"
+drive_colour_x = "#455DC3"
+Signal_color = "#218D8C"
+
+data = pd.read_csv("Fig_5a_SimData_1.csv")
+Noisy_data = pd.read_csv("Fig_5a_SimData_2.csv")
+data = [data, Noisy_data]
+Spin_colours = ['#7701A8', '#EE7C50', 'grey', 'darkgrey', 'lightgrey']
+
+Total_No_of_data_points = 833
+
+index = 0
+Frame_no = 0
+
+for i in range(0, Total_No_of_data_points, Skip_lines+1):
+    
+    fig = plt.figure(figsize=(32,8))
+    ax1 = fig.add_subplot(131, projection='3d',computed_zorder=False)
+    
+    ax2 = fig.add_subplot(132)
+    ax2.set_xlabel(f"Time (arb)", fontsize=18)
+    ax2.set_ylabel(f"Z-Projection of Spin Vector", fontsize=18)
+    ax2.tick_params(axis='both', which='major', labelsize=16)
+    ax2.set_ylim(-1, 1)
+    ax2.set_xlim(0, data[0].Time[Total_No_of_data_points])
+
+    ax3 = fig.add_subplot(133)
+    ax3.set_xlabel(f"Time (arb)", fontsize=18)
+    ax3.set_ylabel(f"X-Projection of Spin Vector", fontsize=18)
+    ax3.tick_params(axis='both', which='major', labelsize=16)
+    ax3.set_ylim(-1, 1)
+    ax3.set_xlim(0, data[0].Time[Total_No_of_data_points])   
+    
+    BSfcs.Make_a_pretty_Bloch_sphere(ax1, linestyle, linewidth, ax2 = None)
+    BSfcs.Plot_Bloch_trajectories(i, Signal_color, linewidth, ax1, arrowhead_size, data, drive_colour_x, drive_colour_z, Signal_Max_Y, Spin_colours, ax2, ax3)
+    plt.savefig(f"{output_dir}/frame_{Frame_no}.png")  # Save each frame
+    #plt.show()    
+    plt.close()
+    
+    Frame_no += 1
+    
+filenames = [f"{output_dir}/frame_{index}.png" for index in range(Frame_no)]
+images = [imageio.imread(filename) for filename in filenames]
+imageio.mimsave("Fig_4c.gif", images, duration=50)  # Adjust duration as needed
+    
